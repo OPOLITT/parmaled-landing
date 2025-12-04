c137812b3000dcc3e6ae6053bfaea9d87221166b
Крынин Владимир Юрьевич <krinin.vlad2804@icloud.com>
2025-12-04 11:28:23 +0300

    feat: add spotlights

--- app/data/category.ts
+++ app/data/category.ts
@@ -6,64 +6,64 @@ export const catalogData: CategoryData[] = [
     title: "линейные светодиодные светильники",
     blocks: [
       {
-        article: "PK-XTD-M4800",
-        image: "/category/linear-luminaires/PK-XTD-M4800.png",
+        article: "PL-XTD-M4800",
+        image: "/category/linear-luminaires/PL-XTD-M4800.png",
       },
       {
-        article: "PK-XTD-I4800",
-        image: "/category/linear-luminaires/PK-XTD-I4800.png",
+        article: "PL-XTD-I4800",
+        image: "/category/linear-luminaires/PL-XTD-I4800.png",
       },
       {
-        article: "PK-XTD-T4800",
-        image: "/category/linear-luminaires/PK-XTD-T4800.png",
+        article: "PL-XTD-T4800",
+        image: "/category/linear-luminaires/PL-XTD-T4800.png",
       },
       {
-        article: "PK-XTD-Y4800",
-        image: "/category/linear-luminaires/PK-XTD-Y4800.png",
+        article: "PL-XTD-Y4800",
+        image: "/category/linear-luminaires/PL-XTD-Y4800.png",
       },
       {
-        article: "PK-XTD-X4800",
-        image: "/category/linear-luminaires/PK-XTD-X4800.png",
+        article: "PL-XTD-X4800",
+        image: "/category/linear-luminaires/PL-XTD-X4800.png",
       },
       {
-        article: "PK-XTD-K1801",
-        image: "/category/linear-luminaires/PK-XTD-K1801.png",
+        article: "PL-XTD-K1801",
+        image: "/category/linear-luminaires/PL-XTD-K1801.png",
       },
       {
-        article: "PK-XQD-I2401",
-        image: "/category/linear-luminaires/PK-XQD-I2401.png",
+        article: "PL-XQD-I2401",
+        image: "/category/linear-luminaires/PL-XQD-I2401.png",
       },
       {
-        article: "PK-XQD-M1801",
-        image: "/category/linear-luminaires/PK-XQD-M1801.png",
+        article: "PL-XQD-M1801",
+        image: "/category/linear-luminaires/PL-XQD-M1801.png",
       },
       {
-        article: "PK-XQD-J1801",
-        image: "/category/linear-luminaires/PK-XQD-J1801.png",
+        article: "PL-XQD-J1801",
+        image: "/category/linear-luminaires/PL-XQD-J1801.png",
       },
       {
-        article: "PK-XQD-K1801",
-        image: "/category/linear-luminaires/PK-XQD-K1801.png",
+        article: "PL-XQD-K1801",
+        image: "/category/linear-luminaires/PL-XQD-K1801.png",
       },
       {
-        article: "PK-XQD-A3601",
-        image: "/category/linear-luminaires/PK-XQD-A3601.png",
+        article: "PL-XQD-A3601",
+        image: "/category/linear-luminaires/PL-XQD-A3601.png",
       },
       {
-        article: "PK-XQD-N1801",
-        image: "/category/linear-luminaires/PK-XQD-N1801.png",
+        article: "PL-XQD-N1801",
+        image: "/category/linear-luminaires/PL-XQD-N1801.png",
       },
       {
-        article: "PK-XQD-K1803",
-        image: "/category/linear-luminaires/PK-XQD-K1803.png",
+        article: "PL-XQD-K1803",
+        image: "/category/linear-luminaires/PL-XQD-K1803.png",
       },
       {
-        article: "PK-XQD-K4801",
-        image: "/category/linear-luminaires/PK-XQD-K4801.png",
+        article: "PL-XQD-K4801",
+        image: "/category/linear-luminaires/PL-XQD-K4801.png",
       },
       {
-        article: "PK-XQD-K10801",
-        image: "/category/linear-luminaires/PK-XQD-K10801.png",
+        article: "PL-XQD-K10801",
+        image: "/category/linear-luminaires/PL-XQD-K10801.png",
       },
     ],
   },
@@ -72,64 +72,184 @@ export const catalogData: CategoryData[] = [
     title: "светодиодные прожекторы",
     blocks: [
       {
-        article: "PL-SPT-001",
-        image: "/products/testpr.png",
+        article: "PL-TGD-10603",
+        image: "/category/spotlights/PL-TGD-10603.png",
       },
       {
-        article: "PL-SPT-002",
-        image: "/products/testpr.png",
+        article: "PL-TGD-11203",
+        image: "/category/spotlights/PL-TGD-11203.png",
       },
       {
-        article: "PL-SPT-003",
-        image: "/products/testpr.png",
+        article: "PL-TGD-11803",
+        image: "/category/spotlights/PL-TGD-11803.png",
       },
       {
-        article: "PL-SPT-004",
-        image: "/products/testpr.png",
+        article: "PL-TGD-13603",
+        image: "/category/spotlights/PL-TGD-13603.png",
       },
       {
-        article: "PL-SPT-005",
-        image: "/products/testpr.png",
+        article: "PL-TGD-17203",
+        image: "/category/spotlights/PL-TGD-17203.png",
       },
       {
-        article: "PL-SPT-006",
-        image: "/products/testpr.png",
+        article: "PL-TGD-K0303",
+        image: "/category/spotlights/PL-TGD-K0303.png",
       },
       {
-        article: "PL-SPT-007",
-        image: "/products/testpr.png",
+        article: "PL-TGD-K0603",
+        image: "/category/spotlights/PL-TGD-K0603.png",
       },
       {
-        article: "PL-SPT-008",
-        image: "/products/testpr.png",
+        article: "PL-TGD-K1203",
+        image: "/category/spotlights/PL-TGD-K1203.png",
       },
       {
-        article: "PL-SPT-009",
-        image: "/products/testpr.png",
+        article: "PL-TGD-K1803",
+        image: "/category/spotlights/PL-TGD-K1803.png",
       },
       {
-        article: "PL-SPT-010",
-        image: "/products/testpr.png",
+        article: "PL-TGD-KA0603",
+        image: "/category/spotlights/PL-TGD-KA0603.png",
       },
       {
-        article: "PL-SPT-011",
-        image: "/products/testpr.png",
+        article: "PL-TGD-KA1203",
+        image: "/category/spotlights/PL-TGD-KA1203.png",
       },
       {
-        article: "PL-SPT-012",
-        image: "/products/testpr.png",
+        article: "PL-TGD-KA1803",
+        image: "/category/spotlights/PL-TGD-KA1803.png",
       },
       {
-        article: "PL-SPT-013",
-        image: "/products/testpr.png",
+        article: "PL-TGD-M7203",
+        image: "/category/spotlights/PL-TGD-M7203.png",
       },
       {
-        article: "PL-SPT-014",
-        image: "/products/testpr.png",
+        article: "PL-TGD-L3601",
+        image: "/category/spotlights/PL-TGD-L3601.png",
       },
       {
-        article: "PL-SPT-015",
-        image: "/products/testpr.png",
+        article: "PL-TGD-L7201",
+        image: "/category/spotlights/PL-TGD-L7201.png",
+      },
+      {
+        article: "PL-TGD-L10801",
+        image: "/category/spotlights/PL-TGD-L10801.png",
+      },
+      {
+        article: "PL-TGD-L14401",
+        image: "/category/spotlights/PL-TGD-L14401.png",
+      },
+      {
+        article: "PL-TGD-L21601",
+        image: "/category/spotlights/PL-TGD-L21601.png",
+      },
+      {
+        article: "PL-TGD-L28801",
+        image: "/category/spotlights/PL-TGD-L28801.png",
+      },
+      {
+        article: "PL-TGD-JA1603",
+        image: "/category/spotlights/PL-TGD-JA1603.png",
+      },
+      {
+        article: "PL-TGD-JA3203",
+        image: "/category/spotlights/PL-TGD-JA3203.png",
+      },
+      {
+        article: "PL-TGD-JA4803",
+        image: "/category/spotlights/PL-TGD-JA4803.png",
+      },
+      {
+        article: "PL-TGD-JA6403",
+        image: "/category/spotlights/PL-TGD-JA6403.png",
+      },
+      {
+        article: "PL-TGD-JB9603",
+        image: "/category/spotlights/PL-TGD-JB9603.png",
+      },
+      {
+        article: "PL-TGD-JB12803",
+        image: "/category/spotlights/PL-TGD-JB12803.png",
+      },
+      {
+        article: "PL-TGD-H0901",
+        image: "/category/spotlights/PL-TGD-H0901.png",
+      },
+      {
+        article: "PL-TGD-H1801",
+        image: "/category/spotlights/PL-TGD-H1801.png",
+      },
+      {
+        article: "PL-TGD-H2401",
+        image: "/category/spotlights/PL-TGD-H2401.png",
+      },
+      {
+        article: "PL-TGD-H3601",
+        image: "/category/spotlights/PL-TGD-H3601.png",
+      },
+      {
+        article: "PL-TGD-H4801",
+        image: "/category/spotlights/PL-TGD-H4801.png",
+      },
+      {
+        article: "PL-TGD-H7201",
+        image: "/category/spotlights/PL-TGD-H7201.png",
+      },
+      {
+        article: "PL-TGD-H9601",
+        image: "/category/spotlights/PL-TGD-H9601.png",
+      },
+      {
+        article: "PL-TGD-H12001",
+        image: "/category/spotlights/PL-TGD-H12001.png",
+      },
+      {
+        article: "PL-TGD-H14401",
+        image: "/category/spotlights/PL-TGD-H14401.png",
+      },
+      {
+        article: "PL-TGD-H16801",
+        image: "/category/spotlights/PL-TGD-H16801.png",
+      },
+      {
+        article: "PL-TGD-CA3601",
+        image: "/category/spotlights/PL-TGD-CA3601.png",
+      },
+      {
+        article: "PL-TGD-CA7201",
+        image: "/category/spotlights/PL-TGD-CA7201.png",
+      },
+      {
+        article: "PL-TGD-CB7201",
+        image: "/category/spotlights/PL-TGD-CB7201.png",
+      },
+      {
+        article: "PL-TGD-CB14401",
+        image: "/category/spotlights/PL-TGD-CB14401.png",
+      },
+      {
+        article: "PL-TGD-DA7201",
+        image: "/category/spotlights/PL-TGD-DA7201.png",
+      },
+      {
+        article: "PL-TGD-DA10801",
+        image: "/category/spotlights/PL-TGD-DA10801.png",
+      },
+      {
+        article: "PL-TGD-DA14401",
+        image: "/category/spotlights/PL-TGD-DA14401.png",
+      },
+      {
+        article: "PL-TGD-DB14401",
+        image: "/category/spotlights/PL-TGD-DB14401.png",
+      },
+      {
+        article: "PL-TGD-DB21601",
+        image: "/category/spotlights/PL-TGD-DB21601.png",
+      },
+      {
+        article: "PL-TGD-DB28801",
+        image: "/category/spotlights/PL-TGD-DB28801.png",
       },
     ],
   },
@@ -336,64 +456,92 @@ export const catalogData: CategoryData[] = [
     title: "подземные светодиодные светильники",
     blocks: [
       {
-        article: "PL-UND-001",
-        image: "/products/testpr.png",
+        article: "PL-DMD-L0102",
+        image: "/category/underground/PL-DMD-L0102.png",
       },
       {
-        article: "PL-UND-002",
-        image: "/products/testpr.png",
+        article: "PL-DMD-L0103",
+        image: "/category/underground/PL-DMD-L0103.png",
       },
       {
-        article: "PL-UND-003",
-        image: "/products/testpr.png",
+        article: "PL-DMD-K0105",
+        image: "/category/underground/PL-DMD-K0105.png",
       },
       {
-        article: "PL-UND-004",
-        image: "/products/testpr.png",
+        article: "PL-DMD-K0110",
+        image: "/category/underground/PL-DMD-K0110.png",
       },
       {
-        article: "PL-UND-005",
-        image: "/products/testpr.png",
+        article: "PL-DMD-M0105",
+        image: "/category/underground/PL-DMD-M0105.png",
       },
       {
-        article: "PL-UND-006",
-        image: "/products/testpr.png",
+        article: "PL-DMD-M0109",
+        image: "/category/underground/PL-DMD-M0109.png",
       },
       {
-        article: "PL-UND-007",
-        image: "/products/testpr.png",
+        article: "PL-DMD-M0115",
+        image: "/category/underground/PL-DMD-M0115.png",
       },
       {
-        article: "PL-UND-008",
-        image: "/products/testpr.png",
+        article: "PL-DMD-A0301",
+        image: "/category/underground/PL-DMD-A0301.png",
       },
       {
-        article: "PL-UND-009",
-        image: "/products/testpr.png",
+        article: "PL-DMD-A0901",
+        image: "/category/underground/PL-DMD-A0901.png",
       },
       {
-        article: "PL-UND-010",
-        image: "/products/testpr.png",
+        article: "PL-DMD-A1801",
+        image: "/category/underground/PL-DMD-A1801.png",
       },
       {
-        article: "PL-UND-011",
-        image: "/products/testpr.png",
+        article: "PL-DMD-G0303",
+        image: "/category/underground/PL-DMD-G0303.png",
       },
       {
-        article: "PL-UND-012",
-        image: "/products/testpr.png",
+        article: "PL-DMD-G0902",
+        image: "/category/underground/PL-DMD-G0902.png",
       },
       {
-        article: "PL-UND-013",
-        image: "/products/testpr.png",
+        article: "PL-DMD-G1802",
+        image: "/category/underground/PL-DMD-G1802.png",
       },
       {
-        article: "PL-UND-014",
-        image: "/products/testpr.png",
+        article: "PL-DMD-G3602",
+        image: "/category/underground/PL-DMD-G3602.png",
       },
       {
-        article: "PL-UND-015",
-        image: "/products/testpr.png",
+        article: "PL-DMD-H0402",
+        image: "/category/underground/PL-DMD-H0402.png",
+      },
+      {
+        article: "PL-DMD-H0902",
+        image: "/category/underground/PL-DMD-H0902.png",
+      },
+      {
+        article: "PL-DMD-H1802",
+        image: "/category/underground/PL-DMD-H1802.png",
+      },
+      {
+        article: "PL-DMD-H3602",
+        image: "/category/underground/PL-DMD-H3602.png",
+      },
+      {
+        article: "PL-DMD-D2401",
+        image: "/category/underground/PL-DMD-D2401.png",
+      },
+      {
+        article: "PL-DMD-E2401",
+        image: "/category/underground/PL-DMD-E2401.png",
+      },
+      {
+        article: "PL-DMD-N2401",
+        image: "/category/underground/PL-DMD-N2401.png",
+      },
+      {
+        article: "PL-DMD-I2401",
+        image: "/category/underground/PL-DMD-I2401.png",
       },
     ],
   },
